8e7797c60a5fce775ce7ff24c670c1f166423806
Peter McBride <mrpetermcbride@gmail.com>
2022-06-06 11:07:52 -0700

    Completed beesbeesbees

--- src/Bee.js
+++ src/Bee.js
@@ -1,3 +1,15 @@
 var Bee = function() {
+  Grub.call(this);
+
+  this.age = 5;
+  this.color = 'yellow';
+  this.job = 'keep on growing';
+
+  // this.eat = function () {};
+
 };
 
+// Set the prototype
+Bee.prototype = Object.create(Grub.prototype);
+// Set the constructor
+Bee.prototype.constructor = Bee;
